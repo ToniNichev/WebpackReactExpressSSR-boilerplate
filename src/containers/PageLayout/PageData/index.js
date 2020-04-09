6427fcafffba82b9873b614bc99f3fbf080dac82
ToniNichev <toni.nichev@gmail.com>
2020-04-09 16:33:37 -0400

    Page data adjusments

--- src/containers/PageLayout/PageData/index.js
+++ src/containers/PageLayout/PageData/index.js
@@ -67,15 +67,6 @@ export default {
     layout: [ 
       {
         span: 12,
-        
-        components: [
-          {
-            name: "Header"
-          }
-        ]
-      },
-      {
-        span: 12,
         components:[
           {
             name: "Greetings"
